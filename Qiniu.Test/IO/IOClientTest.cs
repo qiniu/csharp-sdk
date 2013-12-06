bc9b73fb0b8de5278f706cd6dfa0403838502ef0
icattlecoder <icattlecoder@gmail.com>
2013-12-06 10:02:29 +0800

    fix

--- Qiniu.Test/IO/IOClientTest.cs
+++ Qiniu.Test/IO/IOClientTest.cs
@@ -38,20 +38,10 @@ namespace Qiniu.Test.IO
 			Assert.IsTrue (exp, "PutPolicyTest Failure");
 			exp = false;
 
-			policy = new PutPolicy ("lskjd:lskd");
-			policy.ReturnUrl = "returnUrl.com";
-			try{
-				policy.Token ();
-			}catch{
-				exp = true;
-				Assert.IsTrue (true, "PutPolicyTest Failure");
-			}
-			Assert.IsTrue (exp, "PutPolicyTest Failure");
-			exp = false;
 
 			policy = new PutPolicy("bucket");
 			policy.AsyncOps="";
-			policy.CallBackBody="{\"uid\":123}";
+			policy.CallBackBody="uid=123";
 			policy.CallBackUrl="www.qiniu.com";
 			policy.DetectMime = 1;
 			policy.FsizeLimit=4096;
@@ -60,7 +50,7 @@ namespace Qiniu.Test.IO
 			policy.PersistentOps = "avthumb/m3u8/preset/video_16x9_440k";
 			try {
 				string result = policy.ToString();
-				string expect = "{\"scope\":\"bucket\",\"callBackUrl\":\"www.qiniu.com\",\"callBackBody\":\"{\\\"uid\\\":123}\",\"asyncOps\":\"\",\"deadline\":0,\"insertOnly\":1,\"detectMime\":1,\"fsizeLimit\":4096,\"persistentNotifyUrl\":\"www.yourdomain.com/persistentNotifyUrl\",\"persistentOps\":\"avthumb/m3u8/preset/video_16x9_440k\"}";
+				string expect = "{\"scope\":\"bucket\",\"callBackUrl\":\"www.qiniu.com\",\"callBackBody\":\"uid=123\",\"asyncOps\":\"\",\"deadline\":0,\"insertOnly\":1,\"detectMime\":1,\"fsizeLimit\":4096,\"persistentNotifyUrl\":\"www.yourdomain.com/persistentNotifyUrl\",\"persistentOps\":\"avthumb/m3u8/preset/video_16x9_440k\"}";
 				Assert.IsTrue(result==expect,"PutPolicyTest Failure");
 			} catch (Exception ee){
 				Assert.IsTrue (false, "PutPolicyTest Failure");
