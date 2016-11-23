7e9078c564ab6b5140cb8563e6bc63051b811184
LuckyStarry <starry@vip.qq.com>
2016-11-23 13:48:51 +0800

    support chaining calls

---
+++ Qiniu/Util/IPersistentOps.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Qiniu.Util
+{
+    /// <summary>
+    /// 表示该对象是一个预处理操作。
+    /// </summary>
+    public interface IPersistentOps
+    {
+        /// <summary>
+        /// 获取操作字符串。
+        /// </summary>
+        /// <returns>操作字符串。</returns>
+        string GetOpsString();
+    }
+}
---
+++ Qiniu/Util/PersistentOps.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Qiniu.Util
+{
+    /// <summary>
+    /// 预处理操作
+    /// </summary>
+    public abstract class PersistentOps : IPersistentOps
+    {
+        /// <summary>
+        /// 获取操作字符串。
+        /// </summary>
+        /// <returns>操作字符串。</returns>
+        public abstract string GetOpsString();
+    }
+}
