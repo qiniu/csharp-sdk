ca1744417d93259c2b353732077aae8024643f44
fengyunhai <fengyunhai@qiniu.com>
2017-01-10 15:51:59 +0800

    delete-old-files

--- Qiniu/Http/CancellationSignal.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Qiniu.Http
-{
-    /// <summary>
-    /// HTTP请求取消信号
-    /// </summary>
-    /// <returns>true取消，false继续</returns>
-    public delegate bool CancellationSignal();
-}
--- Qiniu/Storage/Persistent/KeyGenerator.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Qiniu.Storage.Persistent
-{
-    /// <summary>
-    /// 分片上传进度文件记录名称
-    /// </summary>
-    /// <returns></returns>
-    public delegate string KeyGenerator();
-}
