12e0185e804c82da58e0e29ab0e9ff8390606a7d
okbang9 <okbang9@aliyun.com>
2021-07-14 15:00:48 +0800

    test ci

--- src/QiniuTests/Storage/ResumableUploaderTests.cs
+++ src/QiniuTests/Storage/ResumableUploaderTests.cs
@@ -47,9 +47,6 @@ namespace Qiniu.Storage.Tests
         [Test]
         public void ResumeUploadFileTest()
         {
-            string ak = AccessKey;
-            string sk = SecretKey;
-            string bkt = Bucket;
             Mac mac = new Mac(AccessKey, SecretKey);
             Random rand = new Random();
             string key = string.Format("UploadFileTest_{0}.dat", rand.Next());
@@ -70,11 +67,6 @@ namespace Qiniu.Storage.Tests
             putPolicy.SetExpires(3600);
             putPolicy.DeleteAfterDays = 1;
             string token = Auth.CreateUploadToken(mac, putPolicy.ToJsonString());
-            string uptoken = token;
-            Console.WriteLine(uptoken);
-            Console.WriteLine(ak);
-            Console.WriteLine(sk);
-            Console.WriteLine(bkt);
 
             Config config = new Config();
             config.UseHttps = true;
