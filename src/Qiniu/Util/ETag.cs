494473f5edfb490e4963f7e3de654919082ed522
lindexi <lindexi_gd@163.com>
2026-03-03 14:42:18 +0800

    按照 gemini-code-assist 代码审查提议，将 try-catch 删除，以便异常直接对外抛出，让开发者能够了解明确的错误信息，而不是返回空字符串进行迷惑开发者

--- src/Qiniu/Util/ETag.cs
+++ src/Qiniu/Util/ETag.cs
@@ -25,56 +25,46 @@ namespace Qiniu.Util
 
             string qetag = "";
 
-            try
+            using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
             {
-                using (FileStream stream = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+                long fileLength = stream.Length;
+                byte[] buffer = new byte[BlockSize];
+                byte[] finalBuffer = new byte[BlockSha1Size + 1];
+                if (fileLength <= BlockSize)
                 {
-                    long fileLength = stream.Length;
-                    byte[] buffer = new byte[BlockSize];
-                    byte[] finalBuffer = new byte[BlockSha1Size + 1];
-                    if (fileLength <= BlockSize)
+                    int readByteCount = stream.Read(buffer, 0, BlockSize);
+                    byte[] readBuffer = new byte[readByteCount];
+                    Array.Copy(buffer, readBuffer, readByteCount);
+
+                    byte[] sha1Buffer = Hashing.CalcSHA1(readBuffer);
+
+                    finalBuffer[0] = 0x16;
+                    Array.Copy(sha1Buffer, 0, finalBuffer, 1, sha1Buffer.Length);
+                }
+                else
+                {
+                    long blockCount = (fileLength % BlockSize == 0) ? (fileLength / BlockSize) : (fileLength / BlockSize + 1);
+                    byte[] sha1AllBuffer = new byte[BlockSha1Size * blockCount];
+
+                    for (int i = 0; i < blockCount; i++)
                     {
                         int readByteCount = stream.Read(buffer, 0, BlockSize);
                         byte[] readBuffer = new byte[readByteCount];
                         Array.Copy(buffer, readBuffer, readByteCount);
 
                         byte[] sha1Buffer = Hashing.CalcSHA1(readBuffer);
-
-                        finalBuffer[0] = 0x16;
-                        Array.Copy(sha1Buffer, 0, finalBuffer, 1, sha1Buffer.Length);
+                        Array.Copy(sha1Buffer, 0, sha1AllBuffer, i * BlockSha1Size, sha1Buffer.Length);
                     }
-                    else
-                    {
-                        long blockCount = (fileLength % BlockSize == 0) ? (fileLength / BlockSize) : (fileLength / BlockSize + 1);
-                        byte[] sha1AllBuffer = new byte[BlockSha1Size * blockCount];
 
-                        for (int i = 0; i < blockCount; i++)
-                        {
-                            int readByteCount = stream.Read(buffer, 0, BlockSize);
-                            byte[] readBuffer = new byte[readByteCount];
-                            Array.Copy(buffer, readBuffer, readByteCount);
+                    byte[] sha1AllBufferSha1 = Hashing.CalcSHA1(sha1AllBuffer);
 
-                            byte[] sha1Buffer = Hashing.CalcSHA1(readBuffer);
-                            Array.Copy(sha1Buffer, 0, sha1AllBuffer, i * BlockSha1Size, sha1Buffer.Length);
-                        }
+                    finalBuffer[0] = 0x96;
+                    Array.Copy(sha1AllBufferSha1, 0, finalBuffer, 1, sha1AllBufferSha1.Length);
 
-                        byte[] sha1AllBufferSha1 = Hashing.CalcSHA1(sha1AllBuffer);
-
-                        finalBuffer[0] = 0x96;
-                        Array.Copy(sha1AllBufferSha1, 0, finalBuffer, 1, sha1AllBufferSha1.Length);
-
-                    }
-                    qetag = Base64.UrlSafeBase64Encode(finalBuffer);
                 }
+                qetag = Base64.UrlSafeBase64Encode(finalBuffer);
             }
-            catch (IOException)
-            {
-                return string.Empty;
-            }
-            catch (UnauthorizedAccessException)
-            {
-                return string.Empty;
-            }
+
 
             return qetag;
         }
